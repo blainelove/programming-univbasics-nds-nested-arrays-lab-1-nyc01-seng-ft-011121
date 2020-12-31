7d0b2d6e9c83aa4abb0bc6f3027a94ae3c6b0ecb
Blaine R Love <blainelove94@gmail.com>
2020-12-31 17:48:26 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,6 +31,7 @@ def sorted_matrix
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
 end
+sorted_matrix.sort
 
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index,
