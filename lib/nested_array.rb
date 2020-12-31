451f8db1af0536b1cf3865206ac6017aa9cbb1ba
Blaine R Love <blainelove94@gmail.com>
2020-12-31 18:03:14 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -37,7 +37,7 @@ end
 def matrix_lookup(matrix, row, column)
   list=[CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
   list[1][1]
-  puts list
+  p list
 
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
